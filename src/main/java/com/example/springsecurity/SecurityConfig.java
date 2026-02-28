e33372786e75555f00ad80cc20acbf899d260b15
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-02-28 12:55:26 +0530

    Form and Bssic Authentication

--- src/main/java/com/example/springsecurity/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.example.springsecurity;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+//This class provides configuration to the application
+@Configuration
+//It enables te security for application
+@EnableWebSecurity
 public class SecurityConfig {
-    SecurityFilterChain
-    http.authorizeHttpRequests((authorize) -> authorize endline
-        .requestMatchers("/api/hi").permitAll()
-        .requestMatchers("/api/admin").hasRole("ADMIN")
-        .requestMatchers("/api/otheruser").hasRole("USER")
-        .anyRequest().authenticated()
-    )
-            .httpBasic(Customizer.withDefaults());
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        //formLogin configures form authentication
+        //http.formLogin(Customizer.withDefaults());
+
+        //httpBasic enables basic authentication
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }
