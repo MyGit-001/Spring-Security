e0afcda482321fb43bfec46449c4c033bdd3d658
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-03-01 23:26:30 +0530

    Adding In-Memory Authentication

--- src/main/java/com/example/springsecurity/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 //This class provides configuration to the application
 @Configuration
@@ -14,11 +18,29 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-        //formLogin configures form authentication
-        //http.formLogin(Customizer.withDefaults());
+        //form authentication
+        http.formLogin(Customizer.withDefaults());
 
-        //httpBasic enables basic authentication
+        //Basic authentication
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+
     }
+
+    //In-Memory Authentication
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails User1 = User.withUsername("User1")
+                                .password("{noop}UserPass")
+                                .roles("USER").build();
+
+        UserDetails Admin = User.withUsername("Admin")
+                                .password("{noop}AdminPass")
+                                .roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(User1 , Admin);
+    }
+
+
+
 }
